204f6753ff5d020b5bee3e0f7879045689f81bfc
AbdullahShabbir-creator <abdullahshabbir309@gmail.com>
2024-10-17 23:04:23 +0500

    Your commit message here

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 function Home() {
+    console.log("Home component rendered");
     return (
         <>
-          <div className='Home mt-1'></div>
-                <div className='Homedata'>
-                   <h2>Enjoy Learning Center </h2>
-                   <p style={{fontSize:'13px'}}>Education its in general sense is a form of learning in the Knowledge, Skills and Habit </p>
-                   <a href="/"  className='btn btn-primary'>Explor More</a>
-                </div>
-                </>
-    )
+        <div className='Home mt-1'></div>
+        <div className='Homedata'>
+          <h2 className='slide-fwd-center'>Enjoy Learning Center</h2>
+          <p style={{ fontSize: '13px' }}>
+            Education in its general sense is a form of learning in the Knowledge, Skills, and Habit
+          </p>
+          <a href="/" className='btn btn-primary'>Explore More</a>
+        </div>
+      </>
+    ) 
 }
-  
+
 export default Home;
